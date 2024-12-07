b7e9bb603e5d8b75ba1ebed53988779f4b5703ff
johnvesslyalti <altijohnvessly@gmail.com>
2024-12-07 16:06:49 +0530

    here i learnt how to how to work with object in react

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import HomeComponent from './Home';
 
 function App() {
   const name = "John Vessly Alti";
+  const student = {
+    name:'John',
+    rollNo:32,
+    age:21
+  }
   return (
     <div className="App">
       <HomeComponent />
       <div className='one'>{name}</div>
       <h2 style={{backgroundColor:'red',textAlign:'center'}}>Here it is the h2 element</h2>
+      <p>{student.name}</p>
+      <p>{student.rollNo}</p>
+      <p>{student.age}</p>
     </div>
   );
 }
