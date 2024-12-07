7d25e6bb62e4ab0516410ae353bb0b5c9a7bf7e0
johnvesslyalti <altijohnvessly@gmail.com>
2024-12-07 15:59:42 +0530

    here i learnt about how to use styles, inline css and declaring variables

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import HomeComponent from './Home';
 
 function App() {
+  const name = "John Vessly Alti";
   return (
     <div className="App">
       <HomeComponent />
+      <div className='one'>{name}</div>
+      <h2 style={{backgroundColor:'red',textAlign:'center'}}>Here it is the h2 element</h2>
     </div>
   );
 }
